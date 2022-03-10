35fc908e1bfc25ad3b740558c6146eee7f094659
Morgan Selbekk <mselbekk11@gmail.com>
2022-03-10 13:16:22 +0000

    First Push
    First Push

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/morgan/Documents/Docusaurus/Docusaurus/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/morgan/Documents/Docusaurus/Docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/morgan/Documents/Docusaurus/Docusaurus/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/morgan/Documents/Docusaurus/Docusaurus/src/css/custom.css'),
+];
---
+++ build/assets/js/f8551aab.e8dbcd01.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1743],{7574:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var a=n(7294),u=n(2773);function c(){return a.createElement(u.Z,null,a.createElement("h1",null,"My React page"),a.createElement("p",null,"This is a React page I can do a bunch of shit here"))}}}]);
